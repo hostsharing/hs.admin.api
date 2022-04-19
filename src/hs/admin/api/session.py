5ef9fbdbef083d3ee08f54df75ff879f97f0e7fc
Lukas Staab <l.staab@posteo.de>
2022-04-19 22:59:24 +0200

    refactored old exceptions, minor changes to doc and spacing

--- src/hs/admin/api/session.py
+++ src/hs/admin/api/session.py
@@ -4,9 +4,6 @@
 from requests import delete, post
 from yaml import safe_load
 
-from .exceptions import LoginError, SessionError
-
-
 
 class Session(object):
     """ This class implements a remote user session.
@@ -17,27 +14,24 @@ class Session(object):
                    'password': password}
         result = post(provider, data=payload)
         if result.status_code != 201:
-            raise LoginError('Acquisition of ticket granting ticket failed.')
+            raise Exception('Session Error: Acquisition of ticket granting ticket failed.')
         self.service = service
         self.tgt = result.headers['location']
         self.user = username
 
-
     def __exit__(self, exc_type, exc_value, traceback):
         delete(self.tgt)
 
-
     def get_ticket(self):
-        """ Returns an one-time ticket for a remote user action """
+        """ Returns a one-time ticket for a remote user action """
 
         payload = {'service': self.service}
         result = post(self.tgt, data=payload)
         if result.status_code != 200:
-            raise SessionError('Acquisition of session ticket failed.')
+            raise Exception('Session Error: Acquisition of session ticket failed.')
         return safe_load(result.text)
 
-
     def get_user(self):
-        """ Returns the user name """
+        """ Returns the name of the user """
 
         return self.user
